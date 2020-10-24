bffc72b778cd5f4219c0accdaa8c16ca03faf326
Mariana Junqueira <69199400+junqueiras@users.noreply.github.com>
2020-10-24 11:42:51 -0300

    adicionando layout tela de registro

--- app/src/main/java/com/example/lancedados/MainActivity.kt
+++ app/src/main/java/com/example/lancedados/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.lancedados
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -12,22 +13,33 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val botaoLancarDados = findViewById<Button>(R.id.lanceDadosBotao)
-//        val valorDado1 = findViewById<TextView>(R.id.dado1)
-//        val valorDado2 = findViewById<TextView>(R.id.dado2)
+        val dado1 = findViewById<ImageView>(R.id.dado1)
+        val dado2 = findViewById<ImageView>(R.id.dado2)
 
-//        botaoLancarDados.setOnClickListener {
-//            valorDado1.text = lancarDados().toString()
-//            valorDado2.text = lancarDados().toString()
-//        }
-
-//        OUTRA FORMA:
-//        findViewById<Button>(R.id.lanceDadosBotao).setOnClickListener {
-//            findViewById<TextView>(R.id.dado1).text = lancarDados().toString()
-//        }
+        botaoLancarDados.setOnClickListener {
+            dado1.setImageResource(lancarDado())
+            dado2.setImageResource(lancarDado())
+        }
     }
 
-    fun lancarDados(): Int{
+    fun lancarDado(): Int {
+        return recuperaFaceDado(gerarNumero())
+    }
+
+    fun recuperaFaceDado(valor: Int): Int{
+        return when(valor){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+    }
+
+    fun gerarNumero(): Int{
         return (1..6).random()
     }
 
 }
+
